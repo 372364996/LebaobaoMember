153c383d351e61f64da35531bc84fb83c6bcee7c
杜鹏 <dupeng.tommy@outlook.com>
2018-08-07 18:57:23 +0800

    +增加用户管理

---
+++ LebaobaoMember/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LebaobaoMember.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult UserList()
+        {
+            return View();
+        }
+        public ActionResult UserDel()
+        {
+            return View();
+        }
+
+        public ActionResult UserAdd()
+        {
+            return View();
+        }
+        public ActionResult UserLevel()
+        {
+            return View();
+        }
+    }
+}
