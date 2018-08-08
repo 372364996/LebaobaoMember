39c597b54ccd3075e2a0c35a790e0cd785c37cb2
杜鹏 <dupeng.tommy@outlook.com>
2018-08-08 18:05:31 +0800

    +单个删除，批量删除

--- LebaobaoMember/Controllers/UserController.cs
+++ LebaobaoMember/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LebaobaoComponents.Domains;
+using LebaobaoMember.Models.PostModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,23 +16,92 @@ namespace LebaobaoMember.Controllers
         {
             return View();
         }
-        public ActionResult UserList(int index=1)
+        public ActionResult UserList(int index = 1)
         {
-            var data = _db.Users.OrderBy(u=>u.Id).ToPagedList(index, 2);
-            return View(data);
+            var userList = _db.Users.Where(u => u.UserStatus == UserStatus.Ok);
+            ViewBag.UserCount = userList.Count();
+            var model = userList.OrderByDescending(u => u.Id).ToPagedList(index, 10);
+            return View(model);
         }
-        public ActionResult UserDel()
+        public ActionResult UserDel(string name, int index = 1)
         {
-            return View();
+            var userList = _db.Users.Where(u => u.UserStatus == UserStatus.Disabled);
+            ViewBag.UserCount = userList.Count();
+            var model = userList.OrderByDescending(u => u.Id).ToPagedList(index, 10);
+            return View(model);
         }
 
         public ActionResult UserAdd()
         {
             return View();
         }
+
         public ActionResult UserLevel()
         {
             return View();
         }
+        #region Post请求
+        /// <summary>
+        /// 添加用户
+        /// </summary>
+        /// <param name="useraddpostmodel">请求参数实体</param>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult UserAdd(UserAddPostModel useraddpostmodel)
+        {
+            try
+            {
+                var user = new Users
+                {
+                    Name = useraddpostmodel.Name,
+                    ChildName = useraddpostmodel.ChildName,
+                    Address = useraddpostmodel.Address,
+                    CreateTime = DateTime.Now,
+                    LastTime = DateTime.Now,
+                    Phone = useraddpostmodel.Phone,
+                    Sex = useraddpostmodel.Sex,
+                    UserStatus = UserStatus.Ok,
+                    UserTypeId = 1
+                };
+                _db.Users.Add(user);
+                _db.SaveChanges();
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                logger.Debug(ex.Message);
+                return Json(new { success = false });
+            }
+
+        }
+        /// <summary>
+        /// 禁用用户
+        /// </summary>
+        /// <param name="id">用户ID</param>
+        ///  <param name="operation">true 恢复用户，false 禁用用户</param>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult UserDelOrReturn(int[] id, bool operation)
+        {
+            try
+            {
+                foreach (var item in id)
+                {
+                    var user = _db.Users.Find(item);
+                    user.UserStatus = operation ? UserStatus.Ok : UserStatus.Disabled;
+                }
+
+                _db.SaveChanges();
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                logger.Debug(ex.Message);
+                return Json(new { success = false });
+            }
+
+
+        }
+        #endregion
     }
 }
---
+++ LebaobaoMember/Models/PostModels/UserAddPostModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LebaobaoMember.Models.PostModels
+{
+    public class UserAddPostModel
+    {
+        public string Name { get; set; }
+        /// <summary>
+        /// 孩子姓名
+        /// </summary>
+        public string ChildName { get; set; }
+        /// <summary>
+        /// 孩子性别
+        /// </summary>
+        public int Sex { get; set; }
+        /// <summary>
+        /// 用户地址
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// 手机号
+        /// </summary>
+        public string Phone { get; set; }
+    }
+}
