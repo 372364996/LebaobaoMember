2fc7ae18b0e16fc420f734e8b4ab3334929702c7
杜鹏 <dupeng.tommy@outlook.com>
2018-12-05 17:27:32 +0800

    +主站完善，上线

---
+++ Lebaobao/Controllers/LebaobaoController.cs
@@ -0,0 +1,43 @@
+﻿using LebaobaoComponents.Domains;
+using LebaobaoComponents.Helpers;
+using LebaobaoComponents.Repositories.Default;
+using log4net;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lebaobao.Controllers
+{
+    public class LebaobaoController : Controller
+    {
+        protected static ILog logger = LogManager.GetLogger(typeof(LebaobaoController));
+        public LebaobaoDbContext _db = new LebaobaoDbContext();
+        private Users user = null;
+
+    
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            string error = Utils.GetRandomString("0123456789", 6);
+            Session["errorcode"] = error;
+            var context = filterContext.HttpContext;
+            string data = "";
+            if (context.Request.Form != null && context.Request.Form.Count > 0)
+            {
+                data = JsonConvert.SerializeObject(context.Request.Form);
+            }
+            string msg = $@"{error}
+URL:{context.Request.Url.ToString()}
+REFER:{(context.Request.UrlReferrer != null ? filterContext.HttpContext.Request.UrlReferrer.ToString() : "NULL")}
+USER:{(context.User.Identity.IsAuthenticated ? context.User.Identity.Name : "NOT AUTH")}
+DATA:{data}
+{Utils.ExceptionToString(filterContext.Exception)}";
+            logger.Error(msg);
+            //发送错误日志Email
+            //Utils.SendErrorLogEmail(context, error, msg);
+            base.OnException(filterContext);
+        }
+    }
+}
---
+++ Lebaobao/Controllers/OrderController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lebaobao.Controllers
+{
+    public class OrderController : LebaobaoController
+    {
+        // GET: Order
+        public ActionResult GetOrderListByUserPhone(string phone)
+        {
+            var list = _db.Orders.Where(u => u.User.Phone == phone).OrderByDescending(o => o.CreateTime).ToList();
+
+            return View(list);
+        }
+    }
+}
